52458f08308b75b107b914a0ae1a428b3a3e3d66
kanechan <kanes.chan@gmail.com>
2022-04-14 09:44:57 -0300

    Funcionalidades CRUD

---
+++ angular13-tarefeiro-api/src/app/tarefeiro/tarefeiro.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TarefeiroComponent } from './tarefeiro.component';
+
+describe('TarefeiroComponent', () => {
+  let component: TarefeiroComponent;
+  let fixture: ComponentFixture<TarefeiroComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TarefeiroComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TarefeiroComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
